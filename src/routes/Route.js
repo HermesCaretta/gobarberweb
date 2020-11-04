cebe7aca5d9df89f8ffe182d932291ce37307e8a
Hermes Caretta <38593591+HermesCaretta@users.noreply.github.com>
2020-11-04 14:53:51 -0300

    something changed

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -1,5 +1,7 @@
 import React from 'react';
+
 import PropTypes from 'prop-types';
+
 import { Route, Redirect } from 'react-router-dom';
 
 import AuthLayout from '~/pages/_layouts/auth';
@@ -9,7 +11,7 @@ import { store } from '~/store';
 
 export default function RouteWrapper({
   component: Component,
-  isPrivate,
+  isPrivate = false,
   ...rest
 }) {
   const { signed } = store.getState().auth;
